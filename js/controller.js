8945854c669a1963b443f3425f04699be7636dde
Sharath Reddy <reddy.sharath19@gmail.com>
2016-01-03 20:54:25 -0500

    updated app: onload shows weather for current location

--- js/controller.js
+++ js/controller.js
@@ -1,17 +1,20 @@
 var myApp= angular.module('myApp',[]);
 myApp.controller('Rendersdata', function renderdata($scope,$http){
- 
- $scope.submit = function() {
-   			$scope.text=  document.getElementById('search-box').value ;
-   			var url = 'http://api.openweathermap.org/data/2.5/weather?q='+ $scope.text +'&appid=73b59da4a0aef80899ab8e4e8c1dae3a';
-
-		    $http.get(url).success(function(data){
-		         $scope.weather = data;
-		         var text = data.weather;
-		         var obj = JSON.stringify(text);
-				$scope.mweather = obj;
-				
-		    })
-   
-  }  
-})
+  	var mylat= 0; var mylong = 0;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            mylat = position.coords.latitude; 
+            mylong = position.coords.longitude;
+            var myurl = 'http://api.openweathermap.org/data/2.5/weather?lat='+ mylat +'&lon='+ mylong+'&appid=2de143494c0b295cca9337e1e96b00e0';
+	 		$http.get(myurl).success(function(data) {
+	        	$scope.weather = data;
+		    });
+        });
+	}
+	$scope.submit = function() {
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q='+ $scope.text +'&appid=73b59da4a0aef80899ab8e4e8c1dae3a';
+	    $http.get(url).success(function(data) {
+	        $scope.weather = data;		
+	    });
+	}  
+});
